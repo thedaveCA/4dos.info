0f11d17d2f37c0ae0ddf6454ad7712046dd7de9b
Dave <github-public@thedave.dev>
2022-11-13 14:44:06 -0700

    Update script and associated files

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 4dos.info
 
-Deployment via Cloudflare Pages, published to [4dos.info](https://4dos.info), with selected files published to [files.4dos.info](https://files.4dos.info/).
+Deployment via Cloudflare Pages, published to [4dos.info](https://4dos.info), with selected files published to [files.4dos.info](https://files.4dos.info/). Requests are redirected as needed.
+
+File extensions listed in `site-vs-files.txt` will be routed to `files.4dos.info`, and `transform-rule.txt` contains a [Cloudflare Redirect rule](https://dash.cloudflare.com/7b6dd6309ba48f3062c2be9f1918a174/4dos.info/rules/redirect-rules).
--- mirror.cmd
+++ mirror.cmd
@@ -1,7 +1,7 @@
 @ECHO OFF
-rclone sync b2:razxtest/4dos.info C:\Users\dave\GitHub\4dos.info\site --transfers 32 --progress -v --delete-excluded --ignore-case --no-update-modtime --modify-window 1s --exclude-from site-vs-files.txt --dry-run
-rclone sync b2:razxtest/4dos.info C:\Users\dave\GitHub\4dos.info\files --transfers 32 --progress -v --delete-excluded --ignore-case --no-update-modtime --modify-window 1s --include-from site-vs-files.txt
-rclone sync b2:razxtest/4dos.info r2:sites/files.4dos.info/ --transfers 32 --progress -v --delete-excluded --ignore-case --no-update-modtime --modify-window 1s --include-from site-vs-files.txt
+rclone sync b2:razxtest/4dos.info C:\Users\dave\GitHub\4dos.info\site --transfers 32 --progress -v --delete-excluded --ignore-case --no-update-modtime --modify-window 1s --fast-list --exclude-from site-vs-files.txt --dry-run
+rclone sync b2:razxtest/4dos.info C:\Users\dave\GitHub\4dos.info\files --transfers 32 --progress -v --delete-excluded --ignore-case --no-update-modtime --modify-window 1s --fast-list --include-from site-vs-files.txt
+rclone sync b2:razxtest/4dos.info r2:sites/files.4dos.info/ --transfers 32 --progress -v --delete-excluded --ignore-case --no-update-modtime --modify-window 1s --fast-list --include-from site-vs-files.txt
 
 ::echo Files redirected from `site` to `files`
 ::for /F %%x in (site-vs-files.txt) do echo %%x
