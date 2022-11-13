6e665ca78cc5734c4ed590a449f367b8f41539b2
Dave <github-public@thedave.dev>
2022-11-13 14:44:46 -0700

    Update mirror.cmd

--- mirror.cmd
+++ mirror.cmd
@@ -3,12 +3,7 @@ rclone sync b2:razxtest/4dos.info C:\Users\dave\GitHub\4dos.info\site --transfer
 rclone sync b2:razxtest/4dos.info C:\Users\dave\GitHub\4dos.info\files --transfers 32 --progress -v --delete-excluded --ignore-case --no-update-modtime --modify-window 1s --fast-list --include-from site-vs-files.txt
 rclone sync b2:razxtest/4dos.info r2:sites/files.4dos.info/ --transfers 32 --progress -v --delete-excluded --ignore-case --no-update-modtime --modify-window 1s --fast-list --include-from site-vs-files.txt
 
-::echo Files redirected from `site` to `files`
-::for /F %%x in (site-vs-files.txt) do echo %%x
-
 echo (http.host eq "4dos.info" and (> transform-rule.txt
 echo ends_with(http.request.uri.path, ".binary")>> transform-rule.txt
-::for /F %%x in (site-vs-files.txt) do echo or ends_with(http.request.uri.path, "%%x")
 for /f "delims=*"  %%x in (site-vs-files.txt) do echo or ends_with(http.request.uri.path, "%%x")>> transform-rule.txt
-::echo or ends_with(http.request.uri.path, ".test")
 echo ))>> transform-rule.txt
